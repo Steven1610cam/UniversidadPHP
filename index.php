f2eed84ec5dcfc58c7760e9d565f5d39bc4c93b6
DANIEL ARROYO <arroyosteven937@gmail.com>
2026-04-14 23:48:26 -0500

    fix: evitar inserciones múltiples al recargar la página

--- index.php
+++ index.php
@@ -2,9 +2,40 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-try {
-    $conn = new PDO("mysql:host=127.0.0.1;dbname=universidad_db", "root", "6860179");
-    echo "Conexión a la base de datos establecida.";
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
+use App\Domain\Entity\Universidad;
+use App\Infrastructure\Persistence\MySQLUniversidadRepository;
+//solucion insert multiple
+if (!isset($_GET['test'])) {
+    echo "Usa ?test=1 en la URL";
+    exit;
+}
+
+
+// conexión
+$conn = new PDO("mysql:host=127.0.0.1;dbname=universidad_db", "root", "6860179");
+
+// repositorio
+$repo = new MySQLUniversidadRepository($conn);
+
+// entidad
+$uni = new Universidad(
+    "Universidad del Atlántico",
+    "Pública",
+    "https://www.uniatlantico.edu.co",
+    "Rector Ejemplo",
+    "info@uniatlantico.edu.co",
+    "Abierto",
+    "3001234567",
+    "Barranquilla",
+    30,
+    5
+);
+
+// guardar
+$result = $repo->save($uni);
+
+if ($result) {
+    echo "Universidad guardada";
+} else {
+    echo "Error al guardar";
 }
